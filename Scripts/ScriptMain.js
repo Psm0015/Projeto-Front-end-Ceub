488f148a85045be7e85b4dd87ab8bf14cdf7d9a2
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-05-22 10:41:26 -0300

    direitos da pagina

--- Scripts/ScriptMain.js
+++ Scripts/ScriptMain.js
@@ -2,5 +2,13 @@ function rolarsolu() {
     window.scroll({
         top: 600,
         behavior: "smooth",
-      });
+    });
 }
+// SCRIPT DIREITOS DA PÁGINA
+function direitopg() {
+    const dataAtual = new Date();
+    const anoAtual = dataAtual.getFullYear();
+    var txt = `Todos os direitos reservados &copy; ${anoAtual} - MotaSoluTec`
+    document.getElementById('direitospg').innerHTML = txt
+}
+direitopg()
