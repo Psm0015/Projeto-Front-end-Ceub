32c7f373fd6ebe89edf9033bec2cb5380b5161fa
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-05-22 11:41:33 -0300

    Formulário de Contrato Validação

--- Scripts/ScriptContrate.js
+++ Scripts/ScriptContrate.js
@@ -1,7 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#phone').inputmask('(99) 9999-9999[9]', { greedy: false });
 });
 
 function enviarbtn() {
-    document.getElementById('fomulario').innerHTML= '<h1>Obrigado pela sua solicitação! Nossa equipe já foi informada e em breve entraremos em contato com você. Agradecemos sua paciência e estamos ansiosos para ajudá-lo.</h1>'
+
+    var formValid = true;
+
+    // Verifica cada campo do formulário
+    $('form input, form textarea').each(function () {
+        if ($(this).val() === '') {
+            formValid = false;
+            $(this).css('background-color', 'lightcoral');
+        } else {
+            $(this).css('background-color', '');
+        }
+    });
+
+    if (!formValid) {
+        document.getElementById('fomulario').innerHTML += '<p class="redcl">Por favor, preencha todos os campos obrigatórios.</p>'
+        return false; // Impede o envio do formulário se houver campos vazios
+    } else {
+        document.getElementById('fomulario').innerHTML = '<h1>Obrigado pela sua solicitação! Nossa equipe já foi informada e em breve entraremos em contato com você. Agradecemos sua paciência e estamos ansiosos para ajudá-lo.</h1>'
+    }
+
+
+
 }
